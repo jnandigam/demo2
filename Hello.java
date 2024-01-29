3c312b6af32a6cb113552975de963fbaf3c8e2ee
Jag Nandigam (MAC) <jnandigam@yahoo.com>
2024-01-29 11:35:17 -0500

    Modify Hello class again

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello USA!");
+        System.out.println("Hello Michigan!");
         System.out.println("Hello West Michigan!");
     }
 }
