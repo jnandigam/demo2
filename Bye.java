6f8ebdaf2bdce1704aca576110a134a42ebb71f7
Jag Nandigam (MAC) <jnandigam@yahoo.com>
2024-02-02 10:35:17 -0500

    Personalize greeting in Bye class

--- Bye.java
+++ Bye.java
@@ -1,6 +1,9 @@
 public class Bye {
     public static void main(String[] args) {
-        System.out.println("Bye World!");
+        if (args.length > 0) {
+            System.out.println("Bye " + args[0] + "!");
+        } else {
+            System.out.println("Bye World!");
+        }
     }
 }
-
